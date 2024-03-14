23f14be4c141d0321846468e28aa663d60956fba
Teddy Valente <teddy.valente@gmail.com>
2024-03-14 12:34:28 +0100

    First push

---
+++ src/index.js
@@ -0,0 +1,351 @@
+
+import SERVICES from './services';
+import './index.css';
+import { debounce } from 'debounce';
+
+/**
+ * @typedef {object} EmbedData
+ * @description Embed Tool data
+ * @property {string} service - service name
+ * @property {string} url - source URL of embedded content
+ * @property {string} embed - URL to source embed page
+ * @property {number} [width] - embedded content width
+ * @property {number} [height] - embedded content height
+ * @property {string} [caption] - content caption
+ */
+/**
+ * @typedef {object} PasteEvent
+ * @typedef {object} HTMLElement
+ * @typedef {object} Service
+ * @description Service configuration object
+ * @property {RegExp} regex - pattern of source URLs
+ * @property {string} embedUrl - URL scheme to embedded page. Use '<%= remote_id %>' to define a place to insert resource id
+ * @property {string} html - iframe which contains embedded content
+ * @property {Function} [id] - function to get resource id from RegExp groups
+ */
+/**
+ * @typedef {object} EmbedConfig
+ * @description Embed tool configuration object
+ * @property {object} [services] - additional services provided by user. Each property should contain Service object
+ */
+
+/**
+ * @class Embed
+ * @classdesc Embed Tool for Editor.js 2.0
+ *
+ * @property {object} api - Editor.js API
+ * @property {EmbedData} _data - private property with Embed data
+ * @property {HTMLElement} element - embedded content container
+ *
+ * @property {object} services - static property with available services
+ * @property {object} patterns - static property with patterns for paste handling configuration
+ */
+export default class Flexframe {
+
+ static get toolbox() {
+    return {
+      title: 'Flexportal iframes',
+      icon: '<svg width="17" height="15" viewBox="0 0 640 512" xmlns="http://www.w3.org/2000/svg"><path d="M278.9 511.5l-61-17.7c-6.4-1.8-10-8.5-8.2-14.9L346.2 8.7c1.8-6.4 8.5-10 14.9-8.2l61 17.7c6.4 1.8 10 8.5 8.2 14.9L293.8 503.3c-1.9 6.4-8.5 10.1-14.9 8.2zm-114-112.2l43.5-46.4c4.6-4.9 4.3-12.7-.8-17.2L117 256l90.6-79.7c5.1-4.5 5.5-12.3.8-17.2l-43.5-46.4c-4.5-4.8-12.1-5.1-17-.5L3.8 247.2c-5.1 4.7-5.1 12.8 0 17.5l144.1 135.1c4.9 4.6 12.5 4.4 17-.5zm327.2.6l144.1-135.1c5.1-4.7 5.1-12.8 0-17.5L492.1 112.1c-4.8-4.5-12.4-4.3-17 .5L431.6 159c-4.6 4.9-4.3 12.7.8 17.2L523 256l-90.6 79.7c-5.1 4.5-5.5 12.3-.8 17.2l43.5 46.4c4.5 4.9 12.1 5.1 17 .6z"/></svg>'
+    };
+  }
+
+  /**
+   * @param {{data: EmbedData, config: EmbedConfig, api: object}}
+   *   data — previously saved data
+   *   config - user config for Tool
+   *   api - Editor.js API
+   *   readOnly - read-only mode flag
+   */
+  constructor({ data, api, readOnly }) {
+    this.api = api;
+    this._data = {};
+    this.element = null;
+    this.readOnly = readOnly;
+
+    this.data = data;
+  }
+
+  /**
+   * @param {EmbedData} data - embed data
+   * @param {RegExp} [data.regex] - pattern of source URLs
+   * @param {string} [data.embedUrl] - URL scheme to embedded page. Use '<%= remote_id %>' to define a place to insert resource id
+   * @param {string} [data.html] - iframe which contains embedded content
+   * @param {number} [data.height] - iframe height
+   * @param {number} [data.width] - iframe width
+   * @param {string} [data.caption] - caption
+   */
+  set data(data) {
+    if (!(data instanceof Object)) {
+      throw Error('Flexframe Tool data should be object');
+    }
+
+    const { service, source, embed, width, height, caption = '' } = data;
+
+    this._data = {
+      service: service || this.data.service,
+      source: source || this.data.source,
+      embed: embed || this.data.embed,
+      width: width || this.data.width,
+      height: height || this.data.height,
+      caption: caption || this.data.caption || '',
+    };
+
+    const oldView = this.element;
+
+    if (oldView) {
+      oldView.parentNode.replaceChild(this.render(), oldView);
+    }
+  }
+
+  /**
+   * @returns {EmbedData}
+   */
+  get data() {
+    if (this.element) {
+      const caption = this.element.querySelector(`.${this.api.styles.input}`);
+
+      this._data.caption = caption ? caption.innerHTML : '';
+    }
+
+    return this._data;
+  }
+
+  /**
+   * Get plugin styles
+   *
+   * @returns {object}
+   */
+  get CSS() {
+    return {
+      baseClass: this.api.styles.block,
+      input: this.api.styles.input,
+      container: 'flexframe-tool',
+      containerLoading: 'flexframe-tool--loading',
+      preloader: 'flexframe-tool__preloader',
+      caption: 'flexframe-tool__caption',
+      url: 'flexframe-tool__url',
+      content: 'flexframe-tool__content',
+    };
+  }
+
+  /**
+   * Render Embed tool content
+   *
+   * @returns {HTMLElement}
+   */
+  render() {
+    if (!this.data.service) {
+      const container = document.createElement('div');
+
+      this.element = container;
+
+      return container;
+    }
+
+    const { html } = Embed.services[this.data.service];
+    const container = document.createElement('div');
+    const caption = document.createElement('div');
+    const template = document.createElement('template');
+    const preloader = this.createPreloader();
+
+    container.classList.add(this.CSS.baseClass, this.CSS.container, this.CSS.containerLoading);
+    caption.classList.add(this.CSS.input, this.CSS.caption);
+
+    container.appendChild(preloader);
+
+    caption.contentEditable = !this.readOnly;
+    caption.dataset.placeholder = this.api.i18n.t('Enter a caption');
+    caption.innerHTML = this.data.caption || '';
+
+    template.innerHTML = html;
+    template.content.firstChild.setAttribute('src', this.data.embed);
+    template.content.firstChild.classList.add(this.CSS.content);
+
+    const embedIsReady = this.embedIsReady(container);
+
+    container.appendChild(template.content.firstChild);
+    container.appendChild(caption);
+
+    embedIsReady
+      .then(() => {
+        container.classList.remove(this.CSS.containerLoading);
+      });
+
+    this.element = container;
+
+    return container;
+  }
+
+  /**
+   * Creates preloader to append to container while data is loading
+   *
+   * @returns {HTMLElement}
+   */
+  createPreloader() {
+    const preloader = document.createElement('preloader');
+    const url = document.createElement('div');
+
+    url.textContent = this.data.source;
+
+    preloader.classList.add(this.CSS.preloader);
+    url.classList.add(this.CSS.url);
+
+    preloader.appendChild(url);
+
+    return preloader;
+  }
+
+  /**
+   * Save current content and return EmbedData object
+   *
+   * @returns {EmbedData}
+   */
+  save() {
+    return this.data;
+  }
+
+  /**
+   * Handle pasted url and return Service object
+   *
+   * @param {PasteEvent} event - event with pasted data
+   */
+  onPaste(event) {
+    const { key: service, data: url } = event.detail;
+
+    const { regex, embedUrl, width, height, id = (ids) => ids.shift() } = Flexframe.services[service];
+    const result = regex.exec(url).slice(1);
+    const embed = embedUrl.replace(/<%= remote_id %>/g, id(result));
+
+    this.data = {
+      service,
+      source: url,
+      embed,
+      width,
+      height,
+    };
+  }
+
+  /**
+   * Analyze provided config and make object with services to use
+   *
+   * @param {EmbedConfig} config - configuration of embed block element
+   */
+  static prepare({ config = {} }) {
+    const { services = {} } = config;
+
+    let entries = Object.entries(SERVICES);
+
+    const enabledServices = Object
+      .entries(services)
+      .filter(([key, value]) => {
+        return typeof value === 'boolean' && value === true;
+      })
+      .map(([ key ]) => key);
+
+    const userServices = Object
+      .entries(services)
+      .filter(([key, value]) => {
+        return typeof value === 'object';
+      })
+      .filter(([key, service]) => Flexframe.checkServiceConfig(service))
+      .map(([key, service]) => {
+        const { regex, embedUrl, html, height, width, id } = service;
+
+        return [key, {
+          regex,
+          embedUrl,
+          html,
+          height,
+          width,
+          id,
+        } ];
+      });
+
+    if (enabledServices.length) {
+      entries = entries.filter(([ key ]) => enabledServices.includes(key));
+    }
+
+    entries = entries.concat(userServices);
+
+    Flexframe.services = entries.reduce((result, [key, service]) => {
+      if (!(key in result)) {
+        result[key] = service;
+
+        return result;
+      }
+
+      result[key] = Object.assign({}, result[key], service);
+
+      return result;
+    }, {});
+
+    Flexframe.patterns = entries
+      .reduce((result, [key, item]) => {
+        result[key] = item.regex;
+
+        return result;
+      }, {});
+  }
+
+  /**
+   * Check if Service config is valid
+   *
+   * @param {Service} config - configuration of embed block element
+   * @returns {boolean}
+   */
+  static checkServiceConfig(config) {
+    const { regex, embedUrl, html, height, width, id } = config;
+
+    let isValid = regex && regex instanceof RegExp &&
+      embedUrl && typeof embedUrl === 'string' &&
+      html && typeof html === 'string';
+
+    isValid = isValid && (id !== undefined ? id instanceof Function : true);
+    isValid = isValid && (height !== undefined ? Number.isFinite(height) : true);
+    isValid = isValid && (width !== undefined ? Number.isFinite(width) : true);
+
+    return isValid;
+  }
+
+  /**
+   * Paste configuration to enable pasted URLs processing by Editor
+   *
+   * @returns {object} - object of patterns which contain regx for pasteConfig
+   */
+  static get pasteConfig() {
+    return {
+      patterns: Flexframe.patterns,
+    };
+  }
+
+  /**
+   * Notify core that read-only mode is supported
+   *
+   * @returns {boolean}
+   */
+  static get isReadOnlySupported() {
+    return true;
+  }
+
+  /**
+   * Checks that mutations in DOM have finished after appending iframe content
+   *
+   * @param {HTMLElement} targetNode - HTML-element mutations of which to listen
+   * @returns {Promise<any>} - result that all mutations have finished
+   */
+  embedIsReady(targetNode) {
+    const PRELOADER_DELAY = 450;
+
+    let observer = null;
+
+    return new Promise((resolve, reject) => {
+      observer = new MutationObserver(debounce(resolve, PRELOADER_DELAY));
+      observer.observe(targetNode, {
+        childList: true,
+        subtree: true,
+      });
+    }).then(() => {
+      observer.disconnect();
+    });
+  }
+}
